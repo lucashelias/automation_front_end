b753b7cf5977f2c31d783d70f4ab0aaa8c1c9fe7
lucaseliastr <lucas.elias@thomsonreuters.com>
2020-12-14 15:13:34 -0300

    COnfigs

--- src/app/config-inicial/config-inicial.component.ts
+++ src/app/config-inicial/config-inicial.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-config-inicial',
   templateUrl: './config-inicial.component.html',
   styleUrls: ['./config-inicial.component.css']
 })
 export class ConfigInicialComponent implements OnInit {
-
-  valida_config_inicial;
-
+  
+  valida_config_inicial: boolean;0
+  nome_arquivo_config : String;
+  
   constructor() { }
-
+  
   ngOnInit(): void {
+    const fs = require('fs');
+  }
+  
+  valida_arquivo(nome_arquivo_config){
+    
+  
+    
+
+    return this.valida_config_inicial
   }
 
 }
